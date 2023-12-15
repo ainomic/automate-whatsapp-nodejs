2dee56d11ce21474092b0f2f7e603ce5f65981b5
Ainomic Dev <contact@ainomic.in>
2023-12-15 12:54:58 +0530

    Add web api to send messages

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const fs = require('fs');
+
 const qrcode = require('qrcode-terminal');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const whatsapp = new Client({
     authStrategy: new LocalAuth()
 });
+
+const express = require('express');
+const app = express();
+const port = 3000;
 
 // whatsapp
 whatsapp.on('qr', qr => {
@@ -18,8 +24,27 @@ whatsapp.on('ready', () => {
 whatsapp.on('message', async message => {
     if (message.body === '!ping') {
         message.reply('pong');
+        console.log(message.from);
     }
 });
 // end whatsapp
 
+app.get('/send-wa-notifications', (req, res) => {
+    var data = fs.readFileSync('test_data.json');
+    data = JSON.parse(data);
+
+    for (var i = 0; i < data.length; i++) {
+        var numbers = data[i].number + '@c.us';
+        var text = "Dear Mr/Mrs " + data[i].number + ", \nwe inform you that " + data[i].text + ". \nThank you";
+
+        whatsapp.sendMessage(numbers, text)
+    }
+
+    res.send(data)
+})
+
 whatsapp.initialize();
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
